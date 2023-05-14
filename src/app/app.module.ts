95a59c7dc535ed1ee879cd09e9410b4020323eb1
Adam Gleeson <S00221503@atu.ie>
2023-05-14 19:40:43 +0100

    APIPageWork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-//import { FoodApiService } from './services/food-api.service';
+import { FoodApiService } from './services/food-api.service';
 import { PagetwoComponent } from './pagetwo/pagetwo.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DatabasePageComponent } from './database-page/database-page.component';
 
 const routes:Routes=[
-  {path:'pagetwo', component:PagetwoComponent},
-  {path:'database-page', component:DatabasePageComponent}
+  {path:'pagetwo', component:PagetwoComponent}, //API Page
+  {path:'database-page', component:DatabasePageComponent} //DataBasePage
 ]
 
 @NgModule({
@@ -22,7 +22,7 @@ const routes:Routes=[
     BrowserModule,
     RouterModule.forRoot(routes)
   ],
-  //providers: [FoodApiService],
+  providers: [FoodApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
